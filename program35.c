07c03d9316afc920d9c02fc2c32d9c2794d204af
praagya16 <tiwaripraagya@gmail.com>
2025-07-09 23:12:07 +0530

    updated program 35

---
+++ program35.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main(){
+    int i,n,arr[n];
+    printf("Enter the no of elements of array 1:");
+    scanf("%d",n);
+    printf("Enter the %d elements:");
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    
+    }
+}
